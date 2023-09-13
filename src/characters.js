cbc74cc5531823b58ceec7a07b87f9ecf126a80b
Lauren <laurendoan7@gmail.com>
2023-09-12 19:52:21 -0500

    Add event listener on Ratings filter

---
+++ src/characters.js
@@ -0,0 +1,10 @@
+const arrCharacters = document.querySelectorAll('.characters div');
+
+// Update list of characters when Ratings filter changes
+addEventListener('input', (event) => {
+    console.log('Ratings filter changed');
+
+    for (let i = 0; i < arrCharacters.length; i++) {
+
+    }
+});
