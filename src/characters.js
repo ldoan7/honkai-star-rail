38d580580e44857d9290b0efee1422575d0669e5
Lauren <laurendoan7@gmail.com>
2023-09-13 18:20:31 -0500

    Dynamically filter list of characters based on user-selected filters

--- src/characters.js
+++ src/characters.js
@@ -1,10 +1,29 @@
 const arrCharacters = document.querySelectorAll('.characters div');
 
-// Update list of characters when Ratings filter changes
+// Update list of characters when filters change
 addEventListener('input', (event) => {
-    console.log('Ratings filter changed');
+    let elemRating = document.getElementById('rating');
+    let strRating = elemRating.value;
+    console.log('Rating: ' + strRating);
+
+    let elemType = document.getElementById('type');
+    let strType = elemType.value;
+    console.log('Type: ' + strType);
+
+    let elemPath = document.getElementById('path');
+    let strPath = elemPath.value;
+    console.log('Path: ' + strPath);
 
     for (let i = 0; i < arrCharacters.length; i++) {
+        let elem = arrCharacters[i];
 
+        // If the character has the selected filter, keep them displayed
+        if ((strRating === 'none' || elem.classList.contains(strRating)) &&
+            (strType === 'none' || elem.classList.contains(strType)) &&
+            (strPath === 'none' || elem.classList.contains(strPath))) {
+            elem.style.display = 'flex';
+        } else {
+            elem.style.display = 'none';
+        }
     }
 });
